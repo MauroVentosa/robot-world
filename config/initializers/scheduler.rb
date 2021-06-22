82ce9c59a8af20b336d383e473cce60b6b9b1fe4
mventosa <ventosa.mauro@gmail.com>
2021-06-22 11:36:54 -0300

    Enable schedules

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,13 +3,13 @@ require "rufus-scheduler"
 scheduler = Rufus::Scheduler.singleton
 
 # Robot Builder
-#scheduler.every "1m", RobotBuilder.new(RobotBuilder::DEFAULT)
+scheduler.every "1m", RobotBuilder.new(RobotBuilder::DEFAULT)
 
 # Robot Builder cleanup
-#scheduler.every "1d", RobotBuilder.new(RobotBuilder::END_OF_DAY)
+scheduler.every "1d", RobotBuilder.new(RobotBuilder::END_OF_DAY)
 
 # Robot Guard Schedule
 #scheduler.every "1m", RobotGuard.new
 
 # Random amount of order changes per hour
-#scheduler.every "1h", RobotChanger.new
+scheduler.every "1h", RobotChanger.new
